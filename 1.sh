2ec5fd683b52cd3aa1e799a58cd1ffc281040c62
atefeharzhangi <83577725+atefeharzhangi@users.noreply.github.com>
2021-06-02 11:31:46 +0430

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,8 @@
+mkdir file 
+chmod 397 file 
+chmod 440 file
+mkdir file1
+chmod a=rx file1
+chmod g=w file1
+chmod r+x file1
+chmod u+g file1
---
+++ 2.sh
@@ -0,0 +1,3 @@
+chmod -v file1
+chmod -f file1
+chmod -c file1
